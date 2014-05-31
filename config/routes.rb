bfa5e4bde951251070dfb0384ab5658ba7a0cb36
Junya Wako <j.wako@j-q.co.jp>
2014-05-31 21:11:45 +0900

    Add top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/', to: 'top#show'
+  resource :top, controller: :top, only: :show
+
   devise_for :users
 
 end
---
+++ spec/views/top/show.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "top/show.html.erb", :type => :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
