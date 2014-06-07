c35b3f2c44281d21a61a2d22669a8f997878b09c
Junya Wako <j.wako@j-q.co.jp>
2014-06-07 19:17:24 +0900

    fix webrtc

--- app/controllers/s/work_controller.rb
+++ app/controllers/s/work_controller.rb
@@ -2,7 +2,7 @@ class S::WorkController < ApplicationController
 	layout "supporter_rtc"
 
   def show
-  	@work = Work.find_by(url_token: params[:url_token])
+  	@work = Work.find_by(url_token: params[:id])
   end
 
 end
--- app/controllers/u/work_controller.rb
+++ app/controllers/u/work_controller.rb
@@ -2,7 +2,7 @@ class U::WorkController < ApplicationController
 	layout "user_rtc"
 
   def index
-  	@work = Work.find_by(url_token: params[:url_token])
+  	@work = Work.find_by(url_token: params[:id])
   end
 
 end
