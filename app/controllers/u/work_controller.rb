70346de095c3f956524bf3cfd528fa3375d0fd14
Junya Wako <j.wako@j-q.co.jp>
2014-06-07 19:30:27 +0900

    fix js

--- app/controllers/u/work_controller.rb
+++ app/controllers/u/work_controller.rb
@@ -2,7 +2,7 @@ class U::WorkController < ApplicationController
 	layout "user_rtc"
 
   def index
-  	@work = Work.find_by(url_token: params[:id])
+  	@work = Work.find_by(url_token: params[:url_token])
   end
 
 end
